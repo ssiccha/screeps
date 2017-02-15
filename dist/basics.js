5a4b32ba8a32e36539953d20310f7b138606bcdb
Sergio Siccha <sergio.siccha@gmail.com>
2017-02-15 02:14:41 +0100

    Refactoring

---
+++ dist/basics.js
@@ -0,0 +1,52 @@
+var harvest = function(creep) {
+    var sources = creep.room.find(FIND_SOURCES);
+    if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+    }
+}
+
+var deliver = function(creep) {
+    var targets = creep.room.find(
+        FIND_STRUCTURES,
+        {
+            filter: (structure) => {
+                return (structure.structureType == STRUCTURE_EXTENSION ||
+                    structure.structureType == STRUCTURE_SPAWN ||
+                    structure.structureType == STRUCTURE_TOWER) &&
+                    structure.energy < structure.energyCapacity;
+            }
+        }
+    );
+    if(targets.length > 0) {
+        if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(
+                targets[0],
+                {visualizePathStyle: {stroke: '#ffffff'}}
+            );
+        }
+    }
+};
+
+var build = function(creep) {
+    var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+    if(targets.length) {
+        if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+        }
+    }
+}
+
+var upgrade = function(creep) {
+    if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(
+            creep.room.controller,
+            {visualizePathStyle: {stroke: '#ffffff'}}
+        );
+    }
+}
+
+module.exports = {
+    deliver: deliver,
+    build: build,
+    harvest: harvest
+}
--- dist/role.harvester.js
+++ dist/role.harvester.js
@@ -1,28 +1,24 @@
+basics = require('basics');
+
 var roleHarvester = {
 
-    /** @param {Creep} creep **/
-    run: function(creep) {
-	    if(creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
-        }
-        else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION ||
-                                structure.structureType == STRUCTURE_SPAWN ||
-                                structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-                    }
-            });
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                }
-            }
-        }
-	}
+/** @param {Creep} creep **/
+run: function(creep) {
+
+    var delivering = creep.memory.delivering;
+    if(delivering && creep.carry.energy == 0) {
+        creep.memory.delivering = false;
+        delivering = false;
+        creep.say('🔄 harvest');
+    }
+    if(!delivering) {
+        basics.harvest(creep);
+    }
+    if(delivering) {
+        basics.deliver(creep);
+    }
+}
+
 };
 
 module.exports = roleHarvester;
