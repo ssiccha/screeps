c11c0d22b94f842d5f990045f682504ddc4f3a03
Sergio Siccha <sergio.siccha@gmail.com>
2017-02-15 02:06:39 +0100

    Add some comments and todos

--- dist/main.js
+++ dist/main.js
@@ -1,14 +1,20 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
+var basics = require('basics');
+
+var ROLE_MINIMA = { harvester: 3, builder: 3, upgrader: 1 }
+var ROLES = ROLE_MINIMA.keys;
 
 module.exports.loop = function () {
 
+    //      TOWERS      //
     var tower = Game.getObjectById('cd43a2e1548c5df5004b6d5e');
     if(tower) {
-        var closestDamagedStructure = tower.pos.findClosestByRange( FIND_STRUCTURES, {
-            filter: (structure) => structure.hits < structure.hitsMax
-        });
+        var closestDamagedStructure
+            = tower.pos.findClosestByRange( FIND_STRUCTURES, {
+                filter: (structure) => structure.hits < structure.hitsMax
+            });
         if(closestDamagedStructure) {
             tower.repair(closestDamagedStructure);
         }
@@ -17,9 +23,12 @@ module.exports.loop = function () {
             tower.attack(closestHostile);
         }
     }
-    
+
+    //      ROLES       //
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
+        // TODO perform upgrader if exit code not successfull
+        var exitCode;
         if(creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
         }
